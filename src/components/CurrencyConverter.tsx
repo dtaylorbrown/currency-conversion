52c50c79de47629479333ca4fc7b0169f9173db3
dtaylorbrown <daniella.taylor92@gmail.com>
2026-09-23 12:49:17 +0100

    fix: to/from fields

--- src/components/CurrencyConverter.tsx
+++ src/components/CurrencyConverter.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useActionState } from "react";
+import { useActionState, useState } from "react";
 
 import { convertCurrency } from "@/actions/convert";
 import usePreviousConversions from "@/hooks/usePreviousConversions";
@@ -15,6 +15,9 @@ const initialState: ConvertState = {};
 
 const CurrencyConverter = ({ currencies }: { currencies: Currency[] }) => {
   const { prevConversions, addConversion } = usePreviousConversions();
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [amount, setAmount] = useState("");
 
   const [state, formAction, isPending] = useActionState(
     async (prevState: ConvertState, formData: FormData) => {
@@ -39,43 +42,60 @@ const CurrencyConverter = ({ currencies }: { currencies: Currency[] }) => {
           your recent conversions.
         </p>
         <form action={formAction} className={styles["converter-form"]}>
-          <select name="from">
-            <option value="">Select currency</option>
-            {currencies.map((currency) => (
-              <option key={currency.short_code} value={currency.short_code}>
-                {currency.name} ({currency.short_code})
-              </option>
-            ))}
-          </select>
-          <select name="to">
-            <option value="">Select currency</option>
-            {currencies.map((currency) => (
-              <option key={currency.short_code} value={currency.short_code}>
-                {currency.name} ({currency.short_code})
-              </option>
-            ))}
-          </select>
-          <input type="number" name="amount" placeholder="Amount" />
+          <fieldset>
+            <label>From:</label>
+            <div>
+              <select
+                name="from"
+                defaultValue={from}
+                onChange={(e) => setFrom(e.target.value)}
+              >
+                <option value="">Select currency</option>
+                {currencies.map((currency) => (
+                  <option key={currency.short_code} value={currency.short_code}>
+                    {currency.name} ({currency.short_code})
+                  </option>
+                ))}
+              </select>
+              <input
+                type="number"
+                name="amount"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+          </fieldset>
+
+          <fieldset>
+            <label>To:</label>
+            <div>
+              <select
+                name="to"
+                defaultValue={to}
+                onChange={(e) => setTo(e.target.value)}
+              >
+                <option value="">Select currency</option>
+                {currencies.map((currency) => (
+                  <option key={currency.short_code} value={currency.short_code}>
+                    {currency.name} ({currency.short_code})
+                  </option>
+                ))}
+              </select>
+              <input
+                type="number"
+                name="amount-to"
+                disabled
+                value={state?.result ? state.result.converted_amount : ""}
+              />
+            </div>
+          </fieldset>
           <button type="submit" disabled={isSubmitDisabled}>
             {isPending ? "Converting..." : "Convert"}
           </button>
         </form>
 
-        {state.result && (
-          <div
-            className={`${styles["conversion-result"]} ${styles["conversion-success"]}`}
-          >
-            <p>
-              {state.result.amount} {state.result.from} ={" "}
-              {state.result.converted_amount} {state.result.to}
-            </p>
-          </div>
-        )}
-
         {state.error && (
-          <div
-            className={`${styles["conversion-result"]} ${styles["conversion-error"]}`}
-          >
+          <div className={styles["conversion-error"]}>
             <p>Error: {state.error}</p>
           </div>
         )}
